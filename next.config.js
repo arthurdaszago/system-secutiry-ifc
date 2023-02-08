7f96a00d11ff89c32593472ccc3c6a958ab8e3b4
Arthur Zago <arthurzago284@gmail.com>
2023-02-08 09:59:00 -0300

    adde base project

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  assetPrefix: 'https://github.com/arthurdaszago/system-secutiry-ifc.git',
+  images: {
+    unoptimized: true,
+  },
+  reactStrictMode: true,
+}
+
+module.exports = nextConfig
