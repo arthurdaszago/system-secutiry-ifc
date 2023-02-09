cdfdfef60739d6e35a8682fa8b5200568dab9e58
Arthur Zago <arthurzago284@gmail.com>
2023-02-09 07:52:31 -0300

    change asset

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  assetPrefix: 'https://github.com/arthurdaszago/system-secutiry-ifc',
-  // assetPrefix: './',
+  // assetPrefix: 'https://github.com/arthurdaszago/system-secutiry-ifc',
+  assetPrefix: './.next',
   images: {
     unoptimized: true,
   },
