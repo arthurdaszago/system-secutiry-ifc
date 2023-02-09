6389b961b7ef38ddaa843906e9fe163d495b765c
Arthur Zago <arthurzago284@gmail.com>
2023-02-09 08:41:00 -0300

    fix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // assetPrefix: 'https://github.com/arthurdaszago/system-secutiry-ifc',
-  assetPrefix: './.next',
+  // assetPrefix: './_next',
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      '/': { page: '/' },
+      '/404': { page: '/404' },
+    }
+  },
+  trailingSlash: true,
+  assetPrefix: '.',
   images: {
     unoptimized: true,
   },
