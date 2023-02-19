ddc961054350583e96b89aae944364526b6e2b6e
Arthur Zago <arthurzago284@gmail.com>
2023-02-19 13:15:05 -0300

    changed paths

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -2,11 +2,11 @@ const path = (path: string, sublink: string) => {
   return `${path}${sublink}`;
 };
 
-const PATH_PODCASTS = "/system-secutiry-ifc/podcasts";
-const PATH_SECURITY = "/system-secutiry-ifc/seguranca";
+const PATH_PODCASTS = "/system-security-ifc/podcasts";
+const PATH_SECURITY = "/system-security-ifc/seguranca";
 
 export const PATH = {
-  root: "/system-secutiry-ifc/",
+  root: "/system-security-ifc/",
 
   security: {
     root: PATH_SECURITY,
