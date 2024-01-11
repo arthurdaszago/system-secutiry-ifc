cf3df308b25dd3c052d269639001fb6da0b7b956
arthurdaszago <arthurzago284@gmail.com>
2024-01-11 11:47:38 -0300

    removed space

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -17,7 +17,6 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
       <Menu />
-
       <Box component="main">
         <Container maxWidth="xl">
           <Component {...pageProps} />
